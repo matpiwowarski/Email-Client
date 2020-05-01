eaf78fd7dc1c61d57affd9b57ec52ae79485322d
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-05-01 14:35:01 -0700

    UserControls folder

---
+++ E-mail Client/UserControls/MailInfoUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E_mail_Client
+{
+    /// <summary>
+    /// Interaction logic for MailInfoUserControl.xaml
+    /// </summary>
+    public partial class MailInfoUserControl : UserControl
+    {
+        public MailInfoUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
